1eeaeb7a8443a6168c04f9d0230ab703c0119cc6
Abdulahi Abdishakur <128142507+Abdulahi143@users.noreply.github.com>
2024-02-05 17:22:49 +0100

    fixed minor error 05/feb/2024 tried 6.5

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,81 +1,24 @@
-// DashboardPage.tsx
+// DashboardHome.tsx
 import React from 'react';
-import WeekSaleProfits from './_component/WeekSaleProfits';
-import WeekRentProfits from './_component/WeekRentProfits';
-import LatestSalesClient from './_component/fetchingClientside/LatestSalesCLient';
-import LatestRentsClients from './_component/LatestRents';
-import OverView from './_component/OverView';
-import EmptyState from '@/app/components/EmptyState';
-import getCurrentUser from '@/app/actions/getCurrentUser';
-import getUsers from '@/app/actions/getUsers';
-import getListings, { IListingsParams } from '@/app/actions/getListings';
+import DashboardPage from './dashboardPage';
+import { IListingsParams } from '@/app/actions/getListings';
 import { ListingType } from '@prisma/client';
-import { SafeListing, SafeUser } from '@/app/types';
+import { SafeListing } from '@/app/types';
 
 
-interface DashboardProps {
+interface DashboardHomeProps {
   searchParams: IListingsParams;
   type: ListingType;
   listings: SafeListing[];
 }
 
-const DashboardPage: React.FC<DashboardProps> = async ({ searchParams, type, listings }) => {
-  const currentUser = await getCurrentUser();
-
-  if (!currentUser) {
-    return (
-      <EmptyState
-        title="Unauthorized"
-        subtitle="Please login"
-      />
-    );
-  }
-
-  const isAdmin: SafeUser | null = currentUser.isAdmin ? currentUser : null;
-
-  if (!isAdmin) {
-    return (
-      <EmptyState
-        title="You are not admin😢"
-        subtitle="Adios👋"
-      />
-    );
-  }
-
-  const [userOwnedListings, users] = await Promise.all([
-    getListings({ userId: currentUser.id }),
-    getUsers()
-  ]);
-
-  const allListings = await getListings({});
+const DashboardHome = async ({searchParams, type, listings}: DashboardHomeProps) => {
 
   return (
-    <div className="pt-12 px-4 flex-1">
-      <div className="flex flex-wrap gap-4">
-        <div className='flex-1'>
-          <WeekSaleProfits />
-        </div>
-        <div className='flex-1'>
-          <WeekRentProfits />
-        </div>
-      </div>
-      <div className="grid grid-cols-1 2xl:grid-cols-2 xl:gap-4 my-4">
-        <LatestSalesClient
-          users={users || null}
-          listings={allListings}
-          isAdmin={isAdmin}
-          displayCount={6}
-        />
-        <LatestRentsClients
-          users={users || null}
-          listings={allListings}
-          isAdmin={isAdmin}
-          displayCount={6}
-        />
-        <OverView />
-      </div>
-    </div>
+    <>
+      <DashboardPage searchParams={searchParams} type={type} listings={listings} />
+    </>
   );
-}
+};
 
-export default DashboardPage;
+export default DashboardHome;
