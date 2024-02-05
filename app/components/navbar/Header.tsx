798e4b676b4e0cbdc3b9cad00a28a03258404e9b
Abdulahi Abdishakur <128142507+Abdulahi143@users.noreply.github.com>
2024-02-05 22:52:00 +0100

    fixed minor error 05/feb/2024 tried 8.6

--- app/components/navbar/Header.tsx
+++ app/components/navbar/Header.tsx
@@ -2,10 +2,8 @@
 import React, { ReactElement, useState } from 'react'
 import Container from '../Container'
 import Logo from './Logo'
-import Search from './Search'
 import UserMenu from './UserMenu'
 import { SafeUser } from '@/app/types'
-import Categories from './Categories'
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -23,7 +21,6 @@ const Navbar: React.FC<NavbarProps> = ({
       <Container>
       <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
         <Logo />
-        <Search />
         <UserMenu currentUser={currentUser}/>
       </div>
       </Container>
