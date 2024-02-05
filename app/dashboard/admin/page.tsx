4be65957583409b81298c7a43a3108b18cfc6ab6
Abdulahi Abdishakur <128142507+Abdulahi143@users.noreply.github.com>
2024-02-05 18:36:24 +0100

    fixed minor error 05/feb/2024 tried 7.2

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -3,8 +3,8 @@ import DashboardPage from './dashboardPage';
 import { IListingsParams } from '@/app/actions/getListings';
 import { ListingType } from '@prisma/client';
 import { SafeListing } from '@/app/types';
-
-
+import Container from "@/app/components/Container";
+import ClientOnly from '@/app/components/ClientOnly';
 interface DashboardHomeProps {
   searchParams: IListingsParams;
   type: ListingType;
@@ -12,14 +12,15 @@ interface DashboardHomeProps {
 }
 
 const Home = async ({searchParams, type, listings}: DashboardHomeProps) => {
-
   return (
     <>
+      <ClientOnly>
+        <Container>
       <DashboardPage searchParams={searchParams} type={type} listings={listings} />
+        </Container>
+      </ClientOnly>
     </>
   );
 };
 
-export default Home
-
-
+export default Home;
