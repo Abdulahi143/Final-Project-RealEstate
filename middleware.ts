b9ade31096ce1fb5e1895063bf7d2ce3db3745bf
Abdulahi Abdishakur <128142507+Abdulahi143@users.noreply.github.com>
2024-02-08 22:58:21 +0100

    middleware modified #2

--- middleware.ts
+++ middleware.ts
@@ -14,9 +14,9 @@ export const config = {
 export default withAuth(
   async function middleware(req) {
     const url = req.nextUrl.pathname;
-    const userRole = (req?.nextauth?.token?.user as User | undefined)?.isAdmin ?? false;
+    const userRole:boolean = (req?.nextauth?.token?.user as User | undefined)?.isAdmin ?? false;
 
-    if (url?.startsWith('/dashboard/admin/')) {
+    if (url?.startsWith('/dashboard/admin')) {
       if (!userRole) {
         return NextResponse.redirect(new URL("/", req.url));
       }
